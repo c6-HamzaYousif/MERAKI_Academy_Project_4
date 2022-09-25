756f2c5aa992ceb2cdb9b97c996d1baa69e5d343
HamzaYousif <yous.hamze@gmail.com>
2022-09-25 21:59:36 +0300

    create users schema

---
+++ backend/models/users.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+
+const usersSchema = new mongoose.Schema({
+
+firstName: {type: String, required: true},
+lastName: {type: String, required: true},
+email: {type: String, required: true, unique: true, lowercase: true}, 
+password: {type: String, required: true},
+age: {type: Number},
+city: {type: String, required: true},
+image: {type: String},
+role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" }
+})
+
+usersSchema.pre("save", async function (){
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
+module.exports = mongoose.model( "User", usersSchema);
