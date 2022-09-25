8cd5d2b412e5d297d435cf4fbe1086819da1b36a
HamzaYousif <yous.hamze@gmail.com>
2022-09-25 21:58:18 +0300

    create roles schema

---
+++ backend/models/roles.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const rolesSchema = new mongoose.Schema({
+
+type: {type: String, required: true, unique: true},
+permissions: [{type: String, required: true}],
+
+})
+
+module.exports = mongoose.model( "Role", rolesSchema);
