c189d2b8108bd714d0887b5db37fbae784ae0a97
HamzaYousif <yous.hamze@gmail.com>
2022-09-25 22:01:28 +0300

    create products schema

---
+++ backend/models/products.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const productsSchema = new mongoose.Schema({
+
+name: {type: String, required: true},
+price: {type: String, required: true},
+type: {type: String},
+availableItems: {type: Number},
+image: {type: String, required: true},
+gender: {type: String},
+size: [{type: String}],
+ageRange: {type: String},
+season: [{type: String}],
+comments: [{type: mongoose.Schema.Types.ObjectId, ref: "Comment" }]
+
+})
+
+module.exports = mongoose.model( "Product", productsSchema);
