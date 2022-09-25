ad734e55e459727ee126e791ff3178b524e006da
HamzaYousif <yous.hamze@gmail.com>
2022-09-25 22:41:30 +0300

    Authentication

---
+++ backend/middlewares/authentication.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken');
+
+const authentication = (req, res, next) => {
+    const providedToken = req.headers.authorization.split(" ")[1];
+    const secret = "HushhhhhhhhhhThisIsASecret"
+    jwt.verify(providedToken, secret, (err, result) => {
+        if(err){
+            res.status(404).json("Forbidden")
+        }
+        else{
+            console.log(result);
+            req.token = result;
+            next()
+        }
+    })
+}
+
+module.exports = authentication;
