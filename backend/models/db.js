e407b29f14239a626fb03642d1413fa6a7f76a12
HamzaYousif <yous.hamze@gmail.com>
2022-09-25 21:54:33 +0300

    1)Connect & Create files

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 // connecting to mongodb
-mongoose.connect('mongodb://localhost:27017/DB_Name').then(
+mongoose.connect('mongodb://localhost:27017/E-commerce-project').then(
   () => {
-    console.log("DB Ready To Use");
+    console.log("ALL SYSTEMS GO");
   },
   (err) => {
     console.log(err);
