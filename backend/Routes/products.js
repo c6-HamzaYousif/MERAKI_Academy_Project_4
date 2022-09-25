bb6dcee17e7fae51f4a63340f84a7abe6abbcd48
HamzaYousif <yous.hamze@gmail.com>
2022-09-25 23:05:27 +0300

    get all products

--- backend/Routes/products.js
+++ backend/Routes/products.js
@@ -2,6 +2,7 @@ const express = require('express');
 const productsRouter = express.Router();
 const {addProduct, updateProduct, deleteProduct, getAllProducts, getProductById} = require('../controllers/products');
 
+//routers
 productsRouter.post('/', addProduct)
 productsRouter.put('/update/:id', updateProduct)
 productsRouter.delete('/:id', deleteProduct)
